70bf44257d1e797c77e97f174dfe514e739b6f6e
stagefrightband <frepat@student.sisd.net>
2024-11-14 20:32:32 -0700

    update

--- JavaScript/login.js
+++
@@ -1,9 +0,0 @@
-var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
-//U2FsdGVkX18ZUVvShFSES21qHsQEqZXMxQ9zgHy+bu0=
-
-var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-//4d657373616765
-
-document.getElementById("demo1").innerHTML = encrypted;
-document.getElementById("demo2").innerHTML = decrypted;
-document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
