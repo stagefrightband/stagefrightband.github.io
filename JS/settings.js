d109177886a03e0fcd1dae3aa13a2c93e8d1439b
stagefrightband <stagefrightband@users.noreply.github.com>
2024-11-27 19:30:58 +0000

    deploy: 5307769b360faeaa60ac2a0d3740aaaa09163a3e

--- JS/settings.js
+++ JS/settings.js
@@ -1,3 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const highContrastToggle = document.getElementById("highcontrast-toggle");
+  const opendyslexicToggle = document.getElementById("opendyslexic-toggle");
+
+  // Function to get cookie value by name
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+
+  // Initialize High Contrast
+  if (
+    document.cookie
+      .split(";")
+      .some((item) => item.trim() === "highcontrast=true")
+  ) {
+    highContrastToggle.checked = true;
+    document.body.classList.add("high-contrast");
+  }
+
+  highContrastToggle.addEventListener("change", function () {
+    const value = this.checked ? "true" : "false";
+    document.cookie = "highcontrast=" + value + "; max-age=31536000; path=/";
+
+    if (this.checked) {
+      document.body.classList.add("high-contrast");
+    } else {
+      document.body.classList.remove("high-contrast");
+    }
+  });
+
+  // Initialize OpenDyslexic
+  if (
+    document.cookie
+      .split(";")
+      .some((item) => item.trim() === "opendyslexic=true")
+  ) {
+    opendyslexicToggle.checked = true;
+    document.body.classList.add("open-dyslexic");
+  }
+
+  opendyslexicToggle.addEventListener("change", function () {
+    const value = this.checked ? "true" : "false";
+    document.cookie =
+      "opendyslexic=" + value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+
+    if (this.checked) {
+      document.body.classList.add("open-dyslexic");
+    } else {
+      document.body.classList.remove("open-dyslexic");
+    }
+  });
+});
+
 // Function to get a cookie by name
 function getCookie(name) {
   const value = `; ${document.cookie}`;
@@ -25,3 +80,4 @@ setInterval(() => {
   toggleClassBasedOnCookie('highcontrast', 'high-contrast');
   toggleClassBasedOnCookie('opendyslexic', 'open-dyslexic');
 }, 1000);
+
