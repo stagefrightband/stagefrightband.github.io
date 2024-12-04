1e07e1815448bb0327871b7bcf2b36d53b7f99e4
stagefrightband <frepat@student.sisd.net>
2024-12-04 15:04:27 -0700

    update

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-console.log('Supabase URL:', supabaseUrl);
-console.log('Supabase Anon Key:', supabaseAnonKey);
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase URL or anon key');
 }
