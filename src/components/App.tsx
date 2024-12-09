1f6a6cc278fc284491d22acd7dd41e1a102d07c6
stagefrightband <frepat@student.sisd.net>
2024-12-09 16:09:17 -0700

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,28 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const Home = lazy(() => import('./Home'));
-const Login = lazy(() => import('./Login'));
-const Settings = lazy(() => import('./Settings'));
-const AboutUs = lazy(() => import('./AboutUs'));
-const ContactUs = lazy(() => import('./ContactUs'));
-const Store = lazy(() => import('./Store'));
-const TourDates = lazy(() => import('./TourDates'));
-const ShoppingCart = lazy(() => import('./ShoppingCart'));
+import Home from './Home';
+import Login from './Login';
+import Settings from './Settings';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import Store from './Store';
+import TourDates from './TourDates';
+import ShoppingCart from './ShoppingCart';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/tourdates" element={<TourDates />} />
-          <Route path="/shoppingcart" element={<ShoppingCart />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/tourdates" element={<TourDates />} />
+        <Route path="/shoppingcart" element={<ShoppingCart />} />
+      </Routes>
     </Router>
   );
 };
